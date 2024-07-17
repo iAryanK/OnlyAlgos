423887e8484bc8e86b5e9e7b8bdad33170e87a14
Aryan <aryaninmail@gmail.com>
2024-07-17 17:45:23 +0530

    minor fixes

--- components/shared/Navbar.tsx
+++ components/shared/Navbar.tsx
@@ -2,12 +2,18 @@ import React from "react";
 import ThemeToggle from "./ThemeToggle";
 import Link from "next/link";
 
-type Props = {};
+type Props = {
+  home?: boolean;
+};
 
-const Navbar = (props: Props) => {
+const Navbar = ({ home }: Props) => {
   return (
-    <nav className="flex justify-between border-b rounded-b-xl py-3 px-5">
-      <Link href="/">
+    <nav
+      className={`flex justify-end items-center rounded-b-xl py-3 px-5 ${
+        !home && "border-b justify-between"
+      }`}
+    >
+      <Link href="/" className={`${home && "hidden"}`}>
         <p className="w-fit text-2xl font-serif font-bold">
           <span className="italic font-normal">Only</span>Algos
         </p>
